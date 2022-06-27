5a340c15f0cd198eaebf0fac01d3d998e33286fc
Omar <omar.assed@gmail.com>
2022-06-27 15:37:23 -0600

    commit tree 87e8340fcd34beccbfcb1a50c1c27e6baad02d9c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!s"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end 
+
+def halve num
+    if num.class == Integer
+        return num / 2
+    end
+        return nil
+end
+
+
